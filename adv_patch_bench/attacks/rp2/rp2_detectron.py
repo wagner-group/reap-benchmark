Fix evaluation bug for synthetic signs.

adv_patch_bench/attacks/rp2/rp2_detectron.py
@@ -111,7 +111,7 @@ class RP2AttackDetectron(rp2_base.RP2AttackModule):
         # pdb.set_trace()
 
         # Loop through each EoT image
-        loss = 0
+        loss: torch.Tensor = torch.zeros(1, device=adv_img.device)
         for tgt_lb, tgt_log, obj_log in zip(
             target_labels, target_logits, obj_logits
         ):
@@ -128,7 +128,8 @@ class RP2AttackDetectron(rp2_base.RP2AttackModule):
                 tgt_lb = torch.zeros_like(tgt_lb) + obj_class
             # If there's no matched gt/prediction, then attack already succeeds.
             # TODO(feature): Appearing or misclassification attacks
-            target_loss, obj_loss = 0, 0
+            target_loss: torch.Tensor = torch.zeros_like(loss)
+            obj_loss: torch.Tensor = torch.zeros_like(loss)
             if len(tgt_log) > 0 and len(tgt_lb) > 0:
                 # Ignore the background class on tgt_log
                 target_loss = F.cross_entropy(tgt_log, tgt_lb, reduction="mean")
@@ -148,7 +149,7 @@ def _get_targets(
     iou_thres: float = 0.1,
     score_thres: float = 0.1,
     use_correct_only: bool = False,
-) -> Tuple[structures.Boxes, torch.Tensor]:
+) -> Tuple[structures.Boxes, torch.Tensor, torch.Tensor, torch.Tensor]:
     """Select a set of targets to attack.
 
     Args:
@@ -189,17 +190,19 @@ def _get_targets(
     gt_classes = [i["instances"].gt_classes for i in inputs]
     objectness_logits = [x.objectness_logits for x in proposals]
 
-    return _filter_positive_proposals(
+    outputs = _filter_positive_proposals(
         proposal_boxes,
         class_logits,
         gt_boxes,
         gt_classes,
-        objectness_logits,
         device=device,
         iou_thres=iou_thres,
         score_thres=score_thres,
         use_correct_only=use_correct_only,
     )
+    outputs.append(class_logits)
+    outputs.append(objectness_logits)
+    return outputs
 
 
 def _get_roi_heads_predictions(
@@ -230,17 +233,16 @@ def _filter_positive_proposals(
     class_logits: List[torch.Tensor],
     gt_boxes: List[structures.Boxes],
     gt_classes: List[torch.Tensor],
-    objectness_logits: List[torch.Tensor],
     **kwargs,
-) -> Tuple[structures.Boxes, torch.Tensor, torch.Tensor]:
-
-    outputs = [[], [], [], []]
+) -> List[List[Any]]:
+    """See _filter_positive_proposals_single()."""
+    outputs = [[], []]
     for inpt in zip(
-        proposal_boxes, class_logits, gt_boxes, gt_classes, objectness_logits
+        proposal_boxes, class_logits, gt_boxes, gt_classes
     ):
         out = _filter_positive_proposals_single(*inpt, **kwargs)
-        for i in range(4):
-            outputs[i].append(out[i])
+        for i, o in enumerate(out):
+            outputs[i].append(o)
     return outputs
 
 
@@ -249,24 +251,21 @@ def _filter_positive_proposals_single(
     class_logits: torch.Tensor,
     gt_boxes: structures.Boxes,
     gt_classes: torch.Tensor,
-    objectness_logits: torch.Tensor,
     device: str = "cuda",
     iou_thres: float = 0.1,
     score_thres: float = 0.1,
     use_correct_only: bool = False,
-) -> Tuple[structures.Boxes, torch.Tensor, torch.Tensor]:
-    """Filter for desired targets for the DAG algo.
+) -> Tuple[structures.Boxes, torch.Tensor]:
+    """Filter for desired targets for the attack.
 
     Args:
-        proposal_boxes: Proposal boxes directly from RPN
-        scores: Softmaxed scores for each proposal box
-        gt_boxes: Ground truth boxes
-        gt_classes: Ground truth classes
+        proposal_boxes: Proposal boxes directly from RPN.
+        scores: Softmaxed scores for each proposal box.
+        gt_boxes: Ground truth boxes.
+        gt_classes: Ground truth classes.
 
     Returns:
-        filtered_target_boxes:
-        corresponding_class_labels:
-        corresponding_scores:
+        Filtered target boxes and corresponding class labels.
     """
     n_proposals: int = len(proposal_boxes)
 
@@ -298,9 +297,4 @@ def _filter_positive_proposals_single(
     else:
         cond = iou_cond
 
-    return (
-        proposal_boxes[cond],
-        paired_gt_classes[cond].to(device),
-        class_logits[cond],
-        objectness_logits[cond],
-    )
+    return (proposal_boxes[cond], paired_gt_classes[cond].to(device))

adv_patch_bench/evaluators/detectron_evaluator.py
@@ -198,12 +198,13 @@ class DetectronEvaluator:
             target_render: Target of rendered image.
         """
         instances: structures.Instances = outputs["instances"]
-        # ious has shape [num_dts, 1]
+        # Filter both dt by class
+        instances = instances[instances.pred_classes == self._obj_class]
+        # Last gt box is the new one we added for synthetic sign
+        # Output of pairwise_iou here has shape [num_dts, 1]
         ious: torch.Tensor = pairwise_iou(
             instances.pred_boxes,
-            target_render["instances"]
-            .gt_boxes[-1]
-            .to(self._device),  # Last new gt bbox is synthetic sign
+            target_render["instances"].gt_boxes[-1].to(self._device)
         )[:, 0]
 
         # Skip empty ious (no overlap)
@@ -211,17 +212,14 @@ class DetectronEvaluator:
             self._syn_idx += 1
             return
 
+        # Find the match with highest IoU. This matches COCO evaluator.
+        max_iou, max_idx = ious.max(0, keepdim=True)
+        matches = max_iou >= self._all_iou_thres
+        # Zero out scores if there's no match (i.e., IoU lower than threshold)
+        scores = instances.scores[max_idx] * matches
         # Save scores and gt-dt matches at each level of IoU thresholds
-        # Find the match with highest IoU and has correct class
-        ious *= instances.pred_classes == self._obj_class
-        matches = ious[None, :] >= self._all_iou_thres[:, None]
-        # Zero out scores that have lowe IoU than threshold
-        scores = instances.scores[None, :] * matches
-        # Select matched dt with highest score at each IoU
-        idx_max_score = scores.argmax(-1)
-        tmp_idx = torch.arange(len(self._all_iou_thres))
-        self.syn_matches[:, self._syn_idx] = matches[tmp_idx, idx_max_score]
-        self.syn_scores[:, self._syn_idx] = scores[tmp_idx, idx_max_score]
+        self.syn_matches[:, self._syn_idx] = matches
+        self.syn_scores[:, self._syn_idx] = scores
         self._syn_idx += 1
 
     def run(self) -> Tuple[List[Dict[str, Any]], Dict[str, Any]]:

print_results_to_csv.py
@@ -155,7 +155,10 @@ def main(args):
                 #     split_hash = "null"
                 # else:
                 #     split_hash = hashes[2].split("split")[1].split(".pkl")[0]
+                if "bbox" not in results:
+                    continue
                 metrics = results["bbox"]
+                print(result_path)
 
                 # Experiment setting identifier for matching clean and attack
                 obj_class = results["obj_class"]
@@ -192,8 +195,8 @@ def main(args):
                 base_sid += f" | {eval_hash}"
 
                 if base_sid not in tp_scores:
-                    tp_scores[eval_hash] = {t: [] for t in range(10)}
-                    fp_scores[eval_hash] = {t: [] for t in range(10)}
+                    tp_scores[base_sid] = {t: [] for t in range(10)}
+                    fp_scores[base_sid] = {t: [] for t in range(10)}
 
                 scores = cls_scores[obj_class]
                 num_gts = scores.shape[1]
@@ -230,8 +233,8 @@ def main(args):
                     print_df_rows[sid]["Recall"] = outputs["recall"] * 100
                     print_df_rows[sid]["AP"] = results["bbox"]["AP"]
                     for t in range(10):
-                        tp_scores[eval_hash][t].extend(scores_full[t][0])
-                        fp_scores[eval_hash][t].extend(scores_full[t][1])
+                        tp_scores[base_sid][t].extend(scores_full[t][0])
+                        fp_scores[base_sid][t].extend(scores_full[t][1])
 
                 # Create DF row for all classes
                 all_class_sid = f"{base_sid} | all"
@@ -291,7 +294,6 @@ def main(args):
         clean_detected = clean_scores[iou_idx] >= CONF_THRES
         adv_detected = adv_scores[iou_idx] >= CONF_THRES
         total = clean_scores.shape[1]
-        print(total)
 
         num_succeed = np.sum(~adv_detected & clean_detected)
         num_clean = np.sum(clean_detected)
@@ -408,7 +410,7 @@ def main(args):
                 aps[t] = _compute_ap_recall(
                     scores, matches, _NUM_SIGNS_PER_CLASS.sum()
                 )["AP"]
-            print_df_rows[sid + "_allw"]["AP"] = np.mean(aps) * 100
+            print_df_rows[sid + " | allw"]["AP"] = np.mean(aps) * 100
 
     print_df_rows = list(print_df_rows.values())
     df = pd.DataFrame.from_records(print_df_rows)