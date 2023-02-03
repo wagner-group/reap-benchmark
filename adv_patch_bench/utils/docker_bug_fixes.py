46829655ad48e645d57e0da2088abadafd4b37c8
Chawin Sitawarin <chawins@berkeley.edu>
2023-02-03 11:26:35 -0800

    Add comment on docker bug fixes

--- adv_patch_bench/utils/docker_bug_fixes.py
+++ adv_patch_bench/utils/docker_bug_fixes.py
@@ -1,14 +1,26 @@
 """Script to ensure backward compatibility with Nvidia PyTorch docker."""
 
+import logging
 import os
 
 AM_I_IN_A_DOCKER_CONTAINER = os.environ.get("AM_I_IN_A_DOCKER_CONTAINER", False)
+logger = logging.getLogger(__name__)
+
 
 # Calling subprocess.check_output() with python version 3.8.10 or lower will
 # raise NotADirectoryError. When torch calls this to call hipconfig, it does
 # not catch this exception but only FileNotFoundError or PermissionError.
 # This hack makes sure that correct exception is raised.
 if AM_I_IN_A_DOCKER_CONTAINER:
+    logger.info(
+        "Environment variable AM_I_IN_A_DOCKER_CONTAINER is set to %s",
+        AM_I_IN_A_DOCKER_CONTAINER,
+    )
+    logger.info(
+        "Making subprocess.check_output() raise FileNotFoundError instead of "
+        "NotADirectoryError..."
+    )
+
     import subprocess
 
     def _hacky_subprocess_fix(*args, **kwargs):
@@ -20,6 +32,7 @@ if AM_I_IN_A_DOCKER_CONTAINER:
 
     subprocess.check_output = _hacky_subprocess_fix
 
+    logger.info("Replacing torch.Tensor.erfinv_ with scipy.special.erfinv...")
     # This floating point (core dumped) error is caused by the function
     # `erfinv_(x)` when initializing SwinTransformer If you don't face this
     # problem, you can safely comment this out.
